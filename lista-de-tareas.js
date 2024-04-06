662da7aaf6062112a203c494f0cc523192b07e0c
aimev31 <aimev312010@gmail.com>
2024-04-05 22:20:41 -0300

    Cambios menores

--- lista-de-tareas.js
+++ lista-de-tareas.js
@@ -5,12 +5,9 @@ const mesaDeTareas = document.querySelector("#mesaDeTareas");
 
 const ingresarTarea = () => {
     let valorIngresor = ingresor.value;
-
-    // Crear un nuevo contenedor de tarea
     const nuevaTarea = document.createElement("div");
     nuevaTarea.classList.add("tareaContenedor");
 
-    // Crear un nuevo checkbox y párrafo para la tarea
     const checkbox = document.createElement("input");
     checkbox.classList.add("checkBox");
     checkbox.type = "checkbox";
@@ -19,26 +16,21 @@ const ingresarTarea = () => {
     parrafo.classList.add("tarea");
     parrafo.textContent = valorIngresor;
 
-    // Agregar el checkbox y el párrafo al contenedor de la tarea
+    
     nuevaTarea.appendChild(checkbox);
     nuevaTarea.appendChild(parrafo);
 
-    // Agregar la nueva tarea al contenedor de tareas
     mesaDeTareas.appendChild(nuevaTarea);
 
-    // Limpiar el campo de ingreso de tareas después de ingresar una tarea
     ingresor.value = "";
 }
 
-// Función para limpiar la mesa de tareas
 const limpiarMesaDeTareas = () => {
-    // Seleccionar todas las tareas y eliminarlas
+
     const tareas = document.querySelectorAll(".tareaContenedor");
     tareas.forEach(tarea => tarea.remove());
 }
 
-// Agregar evento al botón de ingresar tarea
 ingresar.addEventListener("click", ingresarTarea);
 
-// Agregar evento al botón de limpiar mesa de tareas
 limpiar.addEventListener("click", limpiarMesaDeTareas);
